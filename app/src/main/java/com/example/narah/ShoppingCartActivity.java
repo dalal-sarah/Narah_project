1399d7700618f7fcda88b00e971c211cca160552
dalal <sarah.dalal17@gmail.com>
2020-04-21 10:55:55 +0300

    a

--- app/src/main/java/com/example/narah/ShoppingCartActivity.java
+++ app/src/main/java/com/example/narah/ShoppingCartActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.example.narah.Model.Meal;
@@ -25,13 +28,20 @@ public class ShoppingCartActivity extends AppCompatActivity {
 
     @Override
     protected void onStart() {
-        LinearLayout lyroot = findViewById(R.id.lyrootcart);
         super.onStart();
+        LinearLayout lyroot = findViewById(R.id.lyrootcart);
+        TableLayout tbShoppingCart=findViewById( R.id.tbShoppingCart );
+
         Order order = MainActivity.order;
         int size = order.getList().size();
+        Button [] buttons=new Button[size];
+
         for (int i = 0; i < size; i++) {
+            buttons[i]=new Button(this);
+            buttons[i].setText("delete");
+            TableRow tbr=new TableRow(this);
             LinearLayout lyitem = new LinearLayout(this);
-            lyitem.setOrientation(LinearLayout.VERTICAL);
+            lyitem.setOrientation(LinearLayout.HORIZONTAL);
             Meal meal = order.getList().get(i).getMeal();
             Log.println(Log.INFO,"",meal.getName());
             String mealname = meal.getName();
@@ -40,13 +50,22 @@ public class ShoppingCartActivity extends AppCompatActivity {
             TextView txname = new TextView(this);
             txname.setText(mealname);
             TextView txprice = new TextView(this);
-            txprice.setText("" + mealprice);
+            txprice.setText("    " + mealprice);
             TextView txnum = new TextView(this);
-            txnum.setText("" + nummeal);
-            lyitem.addView(txname);
-            lyitem.addView(txprice);
-            lyitem.addView(txnum);
-            lyroot.addView(lyitem);
+            txnum.setText("    " + nummeal+"    ");
+//            lyitem.addView(txname);
+//            lyitem.addView(txprice);
+//            lyitem.addView(txnum);
+            //txname.setWidth();
+            tbr.addView(txname,new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,
+                    TableRow.LayoutParams.WRAP_CONTENT));
+            tbr.addView(txprice);
+            tbr.addView(txnum);
+            tbr.addView(buttons[i]);
+            tbr.setPadding(0,15,0,15);
+            //tbr.setMinimumWidth(10);
+            tbShoppingCart.addView(tbr);
+         //   lyroot.addView(lyitem);
         }
 
     }
